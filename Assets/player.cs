ec4aa08a89d15d8f82f1c52f8da9ff4cafabc08e
TacHoang <hoangvl1505v@gmail.com>
2025-07-09 23:20:31 +0700

    code player

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,10 +1,33 @@
-using System.ComponentModel;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public class play
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
+    private Vector2 movement;
+
+    void Update()
     {
-        int HP = 100;
+        // Nhận input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Animation (nếu có)
+        if (animator != null)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
