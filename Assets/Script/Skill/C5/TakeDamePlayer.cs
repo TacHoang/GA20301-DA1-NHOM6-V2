ccf41e69ddbf1bd7a235ce2e5562cc7a2e5dc82b
NguyenDuyTan <duytannguyen750@gmail.com>
2025-08-09 22:01:30 +0700

    Fix Game Complete, Finish Game

---
+++ Assets/Script/Skill/C5/TakeDamePlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TakeDamePlayer : MonoBehaviour
+{
+    [SerializeField] private float damageAmount = 20f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth player = collision.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.TakeDamage((int)damageAmount);
+                Debug.Log("Người chơi đã bị bẫy gây sát thương: " + damageAmount);
+            }
+        }
+    }
+
+
+}
